24f2ab1b1e9c39cafe59d9dfc52ca6645e8d180f
AFrenchWrench <AFrenchWrench@gmail.com>
2024-11-03 16:45:09 +0330

    made the redundant codes a function

--- Equations/linear equation.py
+++ Equations/linear equation.py
@@ -1,116 +1,77 @@
+def parse_expression(expression, is_right_side=False):
+    bs = []
+    xs = []
+    inc = 0
+    start = 0
+
+    for i in range(1, len(expression)):
+        if expression[i] == "-" or expression[i] == "+":
+            if "x" in expression[:i]:
+                if expression[:i] == "-x":
+                    xs.append(-1 if not is_right_side else 1)
+                elif expression[:i] == "+x" or expression[:i] == "x":
+                    xs.append(1 if not is_right_side else -1)
+                else:
+                    xs.append(int(expression[: i - 1]) * (-1 if is_right_side else 1))
+            else:
+                bs.append(int(expression[:i]) * (-1 if is_right_side else 1))
+            start += i
+            break
+
+    for i in range(start, len(expression)):
+        i += inc
+        try:
+            if expression[i] == "-" or expression[i] == "+":
+                for j in range(i + 1, len(expression)):
+                    if expression[j] == "-" or expression[j] == "+":
+                        if "x" in expression[i:j]:
+                            if expression[i:j] == "-x":
+                                xs.append(-1 if not is_right_side else 1)
+                            elif expression[i:j] == "+x":
+                                xs.append(1 if not is_right_side else -1)
+                            else:
+                                xs.append(
+                                    int(expression[i : j - 1])
+                                    * (-1 if is_right_side else 1)
+                                )
+                        else:
+                            bs.append(
+                                int(expression[i:j]) * (-1 if is_right_side else 1)
+                            )
+                        inc += (j - i) - 1
+                        break
+                    elif (j + 1) == len(expression):
+                        if "x" in expression[i:]:
+                            if expression[i:] == "-x":
+                                xs.append(-1 if not is_right_side else 1)
+                            elif expression[i:] == "+x":
+                                xs.append(1 if not is_right_side else -1)
+                            else:
+                                xs.append(
+                                    int(expression[i:j]) * (-1 if is_right_side else 1)
+                                )
+                        else:
+                            bs.append(
+                                int(expression[i:]) * (-1 if is_right_side else 1)
+                            )
+                        inc += (j - i) - 1
+        except IndexError:
+            break
+
+    return xs, bs
+
+
 equation = input("Enter the equation: ")
 equation = equation.replace(" ", "")
 
 left_part, right_part = equation.split("=")
 
-bs = []
-xs = []
-cs = []
+left_xs, left_bs = parse_expression(left_part)
+right_xs, right_cs = parse_expression(right_part, is_right_side=True)
 
-inc = 0
-start = 0
-
-for i in range(1, len(left_part)):
-    if left_part[i] == "-" or left_part[i] == "+":
-        if "x" in left_part[:i]:
-            if left_part[:i] == "-x":
-                xs.append(-1)
-            elif left_part[:i] == "+x" or left_part[:i] == "x":
-                xs.append(1)
-            else:
-                xs.append(int(left_part[: i - 1]))
-        else:
-            bs.append(int(left_part[:i]))
-        start += i
-        break
-
-
-for i in range(start, len(left_part)):
-    i += inc
-
-    try:
-        if left_part[i] == "-" or left_part[i] == "+":
-            for j in range(i + 1, len(left_part)):
-                if left_part[j] == "-" or left_part[j] == "+":
-                    if "x" in left_part[i:j]:
-                        if left_part[i:j] == "-x":
-                            xs.append(-1)
-                        elif left_part[i:j] == "+x":
-                            xs.append(1)
-                        else:
-                            xs.append(int(left_part[i : j - 1]))
-                    else:
-                        bs.append(int(left_part[i:j]))
-                    inc += (j - i) - 1
-                    break
-                elif (j + 1) == len(left_part):
-                    if "x" in left_part[i:]:
-                        if left_part[i:] == "-x":
-                            xs.append(-1)
-                        elif left_part[i:] == "+x":
-                            xs.append(1)
-                        else:
-                            xs.append(int(left_part[i:j]))
-                    else:
-                        bs.append(int(left_part[i:]))
-                    inc += (j - i) - 1
-    except IndexError:
-        break
-
-inc = 0
-start = 0
-
-for i in range(1, len(right_part)):
-    if right_part[i] == "-" or right_part[i] == "+":
-        if "x" in right_part[:i]:
-            if right_part[:i] == "-x":
-                xs.append(1)
-            elif right_part[:i] == "+x" or right_part[:i] == "x":
-                xs.append(-1)
-            else:
-                xs.append(-int(right_part[: i - 1]))
-        else:
-            cs.append(int(right_part[:i]))
-        start += i
-        break
-
-
-for i in range(start, len(right_part)):
-    i += inc
-
-    try:
-        if right_part[i] == "-" or right_part[i] == "+":
-            for j in range(i + 1, len(right_part)):
-                if right_part[j] == "-" or right_part[j] == "+":
-                    if "x" in right_part[i:j]:
-                        if right_part[i:j] == "-x":
-                            xs.append(1)
-                        elif right_part[i:j] == "+x":
-                            xs.append(-1)
-                        else:
-                            xs.append(-int(right_part[i : j - 1]))
-                    else:
-                        cs.append(int(right_part[i:j]))
-                    inc += (j - i) - 1
-                    break
-                elif (j + 1) == len(right_part):
-                    if "x" in right_part[i:]:
-                        if right_part[i:] == "-x":
-                            xs.append(1)
-                        elif right_part[i:] == "+x":
-                            xs.append(-1)
-                        else:
-                            xs.append(-int(right_part[i:j]))
-                    else:
-                        cs.append(int(right_part[i:]))
-                    inc += (j - i) - 1
-    except IndexError:
-        break
-
-
-a = sum(xs)
-b = sum(bs)
-c = sum(cs)
+a = sum(left_xs + right_xs)
+b = sum(left_bs)
+c = -sum(right_cs)
 
 if a == 0:
     if b != c:
@@ -119,6 +80,5 @@ if a == 0:
         print("Infinite solution")
 else:
     x = (c - b) / a
-
     print(f"{a}x + {b} = {c}")
     print(f"x={x:.2f}")
